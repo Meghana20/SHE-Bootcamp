323b1f904dc330e94c5d708413a4befcf7b457f1
Meghana20 <56468738+Meghana20@users.noreply.github.com>
2021-06-30 22:31:03 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
 function changecontent(){
-	let x=document.getElementByID("Know-more");
-	if (x.style.display== "block") {
+	let x=document.getElementById("Know-more");
+
+	if (x.style.display== "block")
+	{
 		x.style.display= "none"
 	}
 	else{
 		x.style.display= "block"
 	}
+}
+function validation(){
+
+	let x=document.getElementById("name-input").value;
+	let y=document.getElementById("email-input").value;
+	let z=document.getElementById("batch-input").value;
+
+	if(x=="")
+	{
+		document.getElementById("name-validation").style.display="block";
+	}
+
+	if(y=="")
+	{
+		document.getElementById("email-validation").style.display="block";
+	}
+	if(z=="none")
+	{
+		document.getElementById("batch-validation").style.display="block";
+	}
+	if ((x||y||z)=="")
+	{
+	return false;
+    }
+}
